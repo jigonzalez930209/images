65c7d8ef46ca0cda74123aa1fde0cfd09ad147a1
José Iván González Jorge <jigonzalez930209@gmail.com>
2023-06-13 22:06:10 -0300

    feat: Add const

---
+++ src/lib/utils/const.ts
@@ -0,0 +1,12 @@
+import { ImagesAcceptedTypes } from '../interfaces/interfaces'
+
+export const ALL_ACCEPTED_EXT: ImagesAcceptedTypes[] = ['.jpg', '.png', '.tif', '.tiff', '.jpeg', '.bmp']
+
+export const ImagesGridColumns = {
+  '0': 'grid-cols-1',
+  '1': 'grid-cols-2',
+  '2': 'grid-cols-3',
+  '3': 'grid-cols-4',
+  '4': 'grid-cols-6',
+  '6': 'grid-cols-8',
+}
