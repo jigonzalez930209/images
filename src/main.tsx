e523c4118eba6190978fe0424f639b5136f0dd47
José Iván González Jorge <jigonzalez930209@gmail.com>
2023-06-13 22:02:54 -0300

    feat: Add providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+
+import App from './app'
 import './styles.css'
+import { ThemeProvider } from './providers/theme-provider'
+import { INITIAL_STATE, ImagesProvider } from './lib/context/ImagesProvider'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ImagesProvider initialState={INITIAL_STATE}>
+      <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+        <App />
+      </ThemeProvider>
+    </ImagesProvider>
   </React.StrictMode>
 )
