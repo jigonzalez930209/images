307e255980936dc7415419d61b79700b28bdca60
José Iván González Jorge <jigonzalez930209@gmail.com>
2023-06-15 22:32:17 -0300

    feat: Add toast provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,16 @@ import App from './app'
 import './styles.css'
 import { ThemeProvider } from './providers/theme-provider'
 import { INITIAL_STATE, ImagesProvider } from './lib/context/ImagesProvider'
+import { ToastProvider, ToastViewport } from './ui/toast'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ImagesProvider initialState={INITIAL_STATE}>
       <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-        <App />
+        <ToastProvider duration={5000} swipeDirection='right'>
+          <App />
+          <ToastViewport />
+        </ToastProvider>
       </ThemeProvider>
     </ImagesProvider>
   </React.StrictMode>
