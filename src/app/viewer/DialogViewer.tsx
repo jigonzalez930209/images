44de3c1e701216d32e90724e4e74b3e49324be4c
José Iván González Jorge <jigonzalez930209@gmail.com>
2023-06-26 12:18:34 -0300

    feat: Add containers min w and h and refactor

--- src/app/viewer/DialogViewer.tsx
+++ src/app/viewer/DialogViewer.tsx
@@ -6,18 +6,18 @@ import { X } from 'lucide-react'
 
 const DialogViewer = () => {
   const {
-    imagesState: { imageDialog },
-    setImageDialog,
+    imagesState: { imageView: imageDialog },
+    setImageView: setImageDialog,
   } = React.useContext(ImagesContext)
 
   return (
     <Dialog.Root open={Boolean(imageDialog)} onOpenChange={() => setImageDialog(null)}>
-      <Dialog.Portal className='h-fit w-3/4'>
+      <Dialog.Portal className='min-h-max w-3/4'>
         <div className='fixed inset-0 z-10 flex items-start justify-center sm:items-center'>
           <Dialog.Overlay className='fixed inset-0 z-10 bg-background/80 backdrop-blur-sm transition-all duration-100 data-[state=closed]:animate-out data-[state=closed]:fade-out data-[state=open]:fade-in' />
-          <Dialog.Content className='fixed z-10 w-5/6  gap-4 rounded-b-lg border bg-background p-6 shadow-lg animate-in data-[state=open]:fade-in-90 data-[state=open]:slide-in-from-bottom-10 data-[state=open]:sm:slide-in-from-bottom-0'>
+          <Dialog.Content className='fixed flex items-center justify-center z-10 w-5/6  gap-4 rounded-b-lg border bg-background p-6 shadow-lg animate-in data-[state=open]:fade-in-90 data-[state=open]:slide-in-from-bottom-10 data-[state=open]:sm:slide-in-from-bottom-0'>
             <Dialog.Close className='absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity data-[state=open]:bg-accent data-[state=open]:text-muted-foreground hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none'>
-              <X className='h-4 w-4' />
+              <X className='h-6 w-6' />
               <span className='sr-only'>Close</span>
             </Dialog.Close>
             <Viewer />
--- src/app/viewer/Viewer.tsx
+++ src/app/viewer/Viewer.tsx
@@ -1,12 +1,12 @@
 import { TransformWrapper, TransformComponent, ReactZoomPanPinchRef } from 'react-zoom-pan-pinch'
-import { ChevronLeft, ChevronRight, MinusIcon, PlusIcon, X } from 'lucide-react'
+import { ChevronLeft, ChevronRight, Edit2, MinusIcon, PlusIcon, X } from 'lucide-react'
 import React, { useContext, useRef } from 'react'
 
 import { ImagesContext } from 'src/lib/context/ImageContext'
 import { cn } from 'src/lib/utils'
 
 const Controls = ({ zoomIn, zoomOut, resetTransform }) => (
-  <div className='absolute bottom-10 right-2 z-20 flex gap-2'>
+  <div className='absolute bottom-0 left-0 w-full align-middle justify-center scale-75 z-20 flex gap-2'>
     <div
       className='m-0 cursor-pointer rounded-full border bg-gray-200 p-1 hover:bg-gray-300 hover:shadow-lg dark:bg-gray-700 hover:dark:bg-gray-900'
       onClick={() => zoomIn()}
@@ -30,81 +30,106 @@ const Controls = ({ zoomIn, zoomOut, resetTransform }) => (
 
 const Viewer = () => {
   const {
-    imagesState: { imageDialog, files: images },
-    setImageDialog,
+    imagesState: { imageView, files: images },
+    setImageView,
+    setImageEdit,
   } = useContext(ImagesContext)
   const transformComponentRef = useRef<ReactZoomPanPinchRef | null>(null)
+
+  const [imgState, setImgState] = React.useState<{ positionX: number, positionY: number, scale: number } | null>(null)
+
+  const handleEdition = () => {
+    setImageEdit(imageView)
+    setImageView(null)
+  }
 
   const zoomToImage = () => {
     if (transformComponentRef.current) {
       const { zoomToElement } = transformComponentRef.current
-      zoomToElement(imageDialog.id)
+      zoomToElement(imageView.id)
     }
   }
 
   const handleNext = () => {
-    const index = images.findIndex(image => image.id === imageDialog.id)
+    const index = images.findIndex(image => image.id === imageView.id)
     const nextIndex = index + 1
     if (nextIndex < images.length) {
       const nextImage = images[nextIndex]
-      setImageDialog(nextImage)
+      setImageView(nextImage)
     } else {
-      setImageDialog(images[0])
+      setImageView(images[0])
     }
   }
   const handlePrevious = () => {
-    const index = images.findIndex(image => image.id === imageDialog.id)
+    const index = images.findIndex(image => image.id === imageView.id)
     const previousIndex = index - 1
     if (previousIndex >= 0) {
       const previousImage = images[previousIndex]
-      setImageDialog(previousImage)
+      setImageView(previousImage)
     } else {
-      setImageDialog(images[images.length - 1])
+      setImageView(images[images.length - 1])
     }
   }
   React.useEffect(() => {
     zoomToImage()
-  }, [imageDialog?.id])
+  }, [imageView?.id])
 
-  if (!Boolean(imageDialog)) return null
+  if (!Boolean(imageView)) return null
 
   return (
-    <div className='relative w-[100%] items-center justify-center p-2'>
-      <TransformWrapper
-        initialScale={1}
-        centerOnInit
-        centerZoomedOut
-        limitToBounds={false}
-        wheel={{ step: 0.08 }}
-        ref={transformComponentRef}
-      >
-        {utils => (
-          <React.Fragment>
-            <Controls {...utils} />
-            <ChevronLeft
-              className={cn(
-                'absolute left-2 top-1/2 z-20 cursor-pointer rounded-full border bg-gray-200 p-1 hover:bg-gray-300 hover:shadow-lg dark:bg-gray-700 hover:dark:bg-gray-900',
-                images.length <= 1 && 'hidden'
-              )}
-              onClick={handlePrevious}
-            />
-
-            <TransformComponent>
-              <div className='m-auto w-5/6'>
-                <img src={imageDialog.image} alt={imageDialog.image} id={imageDialog.id} />
-              </div>
-            </TransformComponent>
-            <ChevronRight
-              className={cn(
-                'absolute right-2 top-1/2 z-20 cursor-pointer rounded-full border bg-gray-200 p-1 hover:bg-gray-300 hover:shadow-lg dark:bg-gray-700 hover:dark:bg-gray-900',
-                images.length <= 1 && 'hidden'
-              )}
-              onClick={handleNext}
-            />
-          </React.Fragment>
+    <div className='relative p-2 w-full min-h-[50vh] max-h-[90vh]'>
+      <Edit2 className='absolute left-2 top-2 z-40 cursor-pointer border rounded-md bg-gray-200 p-1 hover:bg-gray-300 hover:shadow-lg dark:bg-gray-700 hover:dark:bg-gray-900'
+        onClick={handleEdition}
+      />
+      <ChevronLeft
+        className={cn(
+          'absolute left-2 top-1/2 z-40 cursor-pointer rounded-full border bg-gray-200 p-1 hover:bg-gray-300 hover:shadow-lg dark:bg-gray-700 hover:dark:bg-gray-900',
+          images.length <= 1 && 'hidden'
         )}
-      </TransformWrapper>
-      <div onClick={zoomToImage}>{imageDialog.name}</div>
+        onClick={handlePrevious}
+      />
+      <ChevronRight
+        className={cn(
+          'absolute right-2 top-1/2 z-40 cursor-pointer rounded-full border bg-gray-200 p-1 hover:bg-gray-300 hover:shadow-lg dark:bg-gray-700 hover:dark:bg-gray-900',
+          images.length <= 1 && 'hidden'
+        )}
+        onClick={handleNext}
+      />
+      <div className='flex items-center justify-center w-full h-full min-h-[70vh]'>
+        <TransformWrapper
+          initialScale={1}
+          centerOnInit
+          centerZoomedOut
+          wheel={{ step: 1 }}
+          ref={transformComponentRef}
+          maxScale={50}
+          onTransformed={(_, state) => setImgState(state)}
+        >
+          {utils => (
+            <React.Fragment>
+              <Controls {...utils} />
+              <TransformComponent
+                contentClass='flex items-center justify-center'
+                wrapperClass='flex items-center justify-center'
+                contentStyle={{
+                  width: '100%',
+                  height: '100%',
+                }}
+                wrapperStyle={{
+                  width: '100%',
+                  height: '100%',
+                }}
+              >
+                <img
+                  className='object-scale-down max-h-[85vh] min-h-[70vh] min-w-[50wh] max-w-[85vw]'
+                  src={imageView.image} alt={imageView.image} id={imageView.id} />
+              </TransformComponent>
+            </React.Fragment>
+          )}
+        </TransformWrapper>
+      </div>
+      <div onClick={zoomToImage}>{imageView.name}</div>
+      {imgState && <div>scale: {imgState.scale} X: {imgState.positionX} Y: {imgState.positionY}</div>}
     </div>
   )
 }
